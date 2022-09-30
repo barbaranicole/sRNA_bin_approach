39e6edf59b3645cc92af557da9e12361ca50338e
barbaranicole <b.g.barbara.n@gmail.com>
2022-09-30 14:24:21 -0300

    more plots are added to the script

--- plot_bin_data.R
+++ plot_bin_data.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(gridExtra)
 library(ggbreak) 
 library(stringr)
+
 ################################################ CLUSTER SIZE DATA ################################################
 
 setwd('~/Work/bin_project/data_test/')
@@ -12,10 +13,12 @@ cluster_size.df <- data.frame() #generacion de data frame vacio
 for (file in files) {
   species = str_split_fixed(file,'/',2)[,1] #obtener el nombre de la especie 
   bin_result = str_split_fixed(file,'/',5)[,3] #obtener el nombre del resultado
+  experiment = str_split_fixed(file,'/',5)[,2]
   
   df <- read.delim(file) #lectura del archivo
   df$species = species # seteo del nombre de la especie dentro del dataframe
   df$bin_result = bin_result # seteo del nombre deresultado y parametro dentro del dataframe
+  df$experiment = experiment 
   
   cluster_size.df <- rbind(cluster_size.df, df) #concatenacion de los dataframes 
   
@@ -57,77 +60,143 @@ for (file in files) {
   
 }
 
+
 ################################################ CLUSTER NUMBER PLOTS ################################################
-
-plot_num <-ggplot(data=cluster_num, aes(x=X, y=number)) + geom_bar(stat="identity", fill="steelblue")+ 
-  geom_text(aes(label=number),hjust = -0.5, size=3.5)+ theme_minimal()
-plot_num <- plot_num + coord_flip() + ggtitle('Number of clusters')
-plot_num
-
-
-#plot(1,1, type='n')
-for (species in unique(cluster_num.df$species)) { #loop atraves de las distintas especies
+for (specie in unique(cluster_num.df$species)) { #loop atraves de las distintas especies
   
-  b = 'bin_results' #seleccion de resultados 
-  df <- cluster_num.df[cluster_num.df$species == species &
-                          cluster_num.df$bin_result == b,] #se genera un dataframe especifico para cada especie 
-  num_colors = length(unique(df$X))
+  print(specie)
+  b = 'bin_result_5' #seleccion de resultados 
+  
+  
+  #result =  paste(specie,b, sep = '_')
+  png(paste0(result, '.png') , 480,480)
+  df <- cluster_size.df[cluster_size.df$specie == specie & cluster_size.df$bin_result == b,]
+  tab <- table(df$chr, df$experiment) #obtencion del numero de clusters 
+  tab <- t(tab)
+  
+  num_colors = length(unique(df$experiment))
   colors = colors <- hcl.colors(num_colors, 'Geyser')
-  names(colors) <-  unique(df$X)
-  print(colors)
-  bar <- barplot(df$number, main=species,  beside = TRUE,  names.arg = c(df$X), col = colors)
-  legend("topright", inset = c(-0.2, 0),  names(colors), col=colors, pch=15)
+  names(colors) <-  unique(df$experiment)
   
+  par(mar=c(10,6,2,2))
+  barplot(tab, las=2, beside=T, col=colors,
+          cex.name=.8, main=specie)
+  
+  legend("topright", inset = c(0, 0),  names(colors), col=colors, pch=15, cex=0.7)
+  #dev.off()
 }
+
 
 for (bin_result in unique(cluster_num.df$bin_result)) { #loop atraves de las distintas especies
   
-    #b = 'bin_results' #seleccion de resultados 
-  species= 'Pyricularia_oryzae'
-  df <- cluster_num.df[cluster_num.df$species == species &
-                         cluster_num.df$bin_result == bin_result,] #se genera un dataframe especifico para cada especie 
-  num_colors = length(unique(df$X))
-  colors = colors <- hcl.colors(num_colors, 'Geyser')
-  names(colors) <-  unique(df$X)
-  print(colors)
-  bar <- barplot(df$number, main=paste(species, bin_result),  beside = TRUE,  names.arg = c(df$X), col = colors, las=2, cex.names=0.7)
-  legend("topright", inset = c(-0.1, 0),  names(colors), col=colors, pch=15, cex=0.6)
+  species= 'Arabidopsis_thaliana'
+  df <- cluster_size.df[cluster_size.df$species == species &
+                          cluster_size.df$bin_result == bin_result,] #se genera un dataframe especifico para cada especie 
   
-  unique(df$X)
+  tab <- table(df$chr, df$experiment) #obtencion del numero de clusters 
+  tab <- t(tab)
+  tab
+  num_colors = length(unique(df$experiment))
+  colors = colors <- hcl.colors(num_colors, 'Geyser')
+  names(colors) <-  unique(df$experiment)
+  
+  par(mar=c(8,4,4,10), xpd=T)
+  barplot(tab, las=2, beside=T, col=colors,
+          cex.name=.8, main=paste(species, bin_result))
+  
+  legend("topright", inset = c(-0.3, 0),  names(colors), col=colors, pch=15, cex=0.8)
 }  
 
 
-#set colors
-hcl.pals('qualitative') #paletas que se utilizan de forma cuantitativa
-colors <- hcl.colors(4, 'Dynamic') #obtener colores determinados de una paleta
+############################################### CLUSTER CUMULATIVE SIZE PLOTS ###############################################
+###largo de clusters por resultados y por especie ###
+colors <- hcl.colors(4, 'Geyser') #obtener colores determinados de una paleta
 names(colors) <-  unique(cluster_size.df$species) #darle nombre a los colores seleccionados en colors de acuerdo a la especie
 
-#funcion para generar un plot de distintas especies
-plot_cdf <- function(save=F) {
+b = 'bin_result_01'
+par(mar=c(8,6,4,2), xpd=F)
+plot(1,1, type='n', xlim=c(0,500),ylim=c(0,1), #plot vacio
+     xlab='Cluster length (nucleotides)',
+     ylab="Cumulative proportion",
+     main=b)
+
+for (species in unique(cluster_size.df$species)) { #loop atraves de las distintas especies
   
-  if (save) {png("bin_results.png", 480,480)} #generacion de la imagen para guardar el plot 
+   #seleccion de resultados 
+  df <- cluster_size.df[cluster_size.df$species == species &
+                          cluster_size.df$bin_result == b,] #se genera un dataframe especifico para cada especie 
   
-  plot(1,1, type='n', xlim=c(0,500), ylim=c(0,1), #plot vacio
-       xlab='Cluster length (nucleotides)',
-       ylab="Cumulative proportion",
-       main="Bin_results")
-  
-  for (species in unique(cluster_size.df$species)) { #loop atraves de las distintas especies
-    
-    b = 'bin_results' #seleccion de resultados 
-    df <- cluster_size.df[cluster_size.df$species == species &
-                            cluster_size.df$bin_result == b,] #se genera un dataframe especifico para cada especie 
-    cdf <- ecdf(df$lenght) #funcion de densidad acumulativa del largo de los bins
-    color <- colors[species] #seleccion del color de acuerdo a la especie
-    plot(cdf, add=T, verticals = T, do.points=F, col=color, lwd=2) #plot de la funcion 
-    
-  }
-  
-  legend("bottomright", names(colors), col=colors, pch=19, inset=0.05) #leyenda para mostrar de que color es cada especie  
-  
-  if (save) {dev.off()} #si save es T entonces, cerrara la ventana del png para guardar el plot 
+  cdf <- ecdf(df$lenght) #funcion de densidad acumulativa del largo de los bins
+  color <- colors[species] #seleccion del color de acuerdo a la especie
+  plot(cdf, add=T, verticals = T, do.points=F, col=color, lwd=2) #plot de la funcion 
   
 }
+legend("bottomright", names(colors), col=colors, pch=19, inset=0.05) #leyenda para mostrar de que color es cada especie  
 
-plot_cdf()
 
+###largo de clusters por especie y por resultado ###
+specie = 'Arabidopsis_thaliana'
+par(mar=c(8,6,4,2), xpd=F)
+plot(1,1, type='n', xlim=c(0,500),ylim=c(0,1), #plot vacio
+     xlab='Cluster length (nucleotides)',
+     ylab="Cumulative proportion",
+     main= specie)
+
+colors <- hcl.colors(4, 'Geyser') #obtener colores determinados de una paleta
+names(colors) <-  unique(cluster_size.df$bin_result) 
+
+for (bin_result in unique(cluster_size.df$bin_result)) { #loop atraves de las distintas especies
+  df <- cluster_size.df[cluster_size.df$species == specie &
+                          cluster_size.df$bin_result == bin_result,] #se genera un dataframe especifico para cada especie 
+  cdf <- ecdf(df$lenght) #funcion de densidad acumulativa del largo de los bins
+  color <- colors[bin_result] #seleccion del color de acuerdo a la especie
+  plot(cdf, add=T, verticals = T, do.points=F, col=color, lwd=2) #plot de la funcion 
+}
+legend("bottomright", names(colors), col=colors, pch=19, inset=0.05) #leyenda para mostrar de que color es cada especie  
+
+
+### Distance to next cluster by specie and rpm ###
+specie = 'Botrytis_cinerea'
+
+par(mar=c(8,6,4,2), xpd=F)
+plot(1,1, type='n', xlim=c(0,6000),ylim=c(0,1), #plot vacio
+     xlab='Distance to next cluster (nucleotides)',
+     ylab="Cumulative proportion",
+     main= specie)
+colors <- hcl.colors(4, 'Geyser') #obtener colores determinados de una paleta
+names(colors) <-  unique(cluster_size.df$bin_result) 
+
+for (bin_result in unique(cluster_size.df$bin_result)) { #loop atraves de las distintas especies
+
+  df <- cluster_size.df[cluster_size.df$species == specie &
+                          cluster_size.df$bin_result == bin_result,] #se genera un dataframe especifico para cada especie 
+  cdf <- ecdf(df$distance_next_cluster) #funcion de densidad acumulativa del largo de los bins
+  color <- colors[bin_result] #seleccion del color de acuerdo a la especie
+  plot(cdf, add=T, verticals = T, do.points=F, col=color, lwd=2) #plot de la funcion 
+  
+}
+legend("bottomright", names(colors), col=colors, pch=19, inset=0.05) #leyenda para mostrar de que color es cada especie  
+
+
+specie = 'Botrytis_cinerea'
+bin_result = 'bin_result_01'
+par(mar=c(8,6,4,2), xpd=F)
+plot(1,1, type='n', xlim=c(0,6000),ylim=c(0,1), #plot vacio
+     xlab='Distance to next cluster (nucleotides)',
+     ylab="Cumulative proportion",
+     main= specie)
+colors <- hcl.colors(3, 'Geyser') #obtener colores determinados de una paleta
+names(colors) <-  unique(cluster_size.df[cluster_size.df$species == specie,]$experiment)
+
+for (experiment in unique(cluster_size.df[cluster_size.df$species == specie,]$experiment)) { #loop atraves de los distintos exp
+
+  df <- cluster_size.df[cluster_size.df$species == specie &
+                          cluster_size.df$bin_result == bin_result & 
+                          cluster_size.df$experiment == experiment ,] #se genera un dataframe especifico para cada especie 
+  cdf <- ecdf(df$distance_next_cluster) #funcion de densidad acumulativa del largo de los bins
+  color <- colors[experiment] #seleccion del color de acuerdo a la especie
+  plot(cdf, add=T, verticals = T, do.points=F, col=color, lwd=2) #plot de la funcion 
+  
+}
+legend("bottomright", names(colors), col=colors, pch=19, inset=0.05) #leyenda para mostrar de que color es cada especie  
+ 
